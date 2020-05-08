8c8f6cef1c0c4f0029235ddf7096eefbabcd5fdb
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-07 20:52:24 -0500

    connect to sql database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,13 @@
 var mysql = require("mysql");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "bamazon_db"
+});
+
+connection.connect (function(err){
+    if (err) throw err;
+    console.log("Connected!");
+});
