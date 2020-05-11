bd0e9aed54674f23c15cfa78a72ec7e5f1fba53b
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-10 20:10:58 -0500

    set up database connection and install required packages

---
+++ bamazonManager.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+//create connection to the database
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "bamazon_db"
+});
+
+connection.connect (function(err){
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    connection.end()
+});
