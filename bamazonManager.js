8639b41747ae49a9ab03186d8fa2f888f66ee80e
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-10 20:39:27 -0500

    make viewProducts function

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,5 +12,63 @@ var connection = mysql.createConnection({
 connection.connect (function(err){
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    connection.end()
+    displayOptions()
 });
+
+function displayOptions() {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "What option would you like?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+            name: "options"
+
+        }
+    ]).then(function(answer){
+      
+        if("View Products for Sale"){
+            viewProducts()
+        }
+
+        if("View Low Inventory"){
+            lowInv()
+        }
+
+        if("Add to Inventory"){
+            addInv()
+        }
+
+        if("Add New Product"){
+            addProduct()
+        }
+    })
+
+
+    connection.end()
+}
+
+function viewProducts(){
+     var query = "SELECT * FROM products";
+        connection.query(
+            query, 
+            function(err, res){
+                for(var i = 0; i < res.length; i++){
+                    console.log(`
+                        Product ID: ${res[i].item_id} Product Name: ${res[i].product_name} Product Price: ${res[i].price}
+                    `)
+                }
+            }
+        )
+}
+
+function lowInv(){
+    var query = "SELECT * FROM products";
+        connection.query(
+            query,
+            function(err, res){
+                for(var i = 0; i < res.length)
+            }
+
+        )
+}
