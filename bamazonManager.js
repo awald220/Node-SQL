4a18c81a4358a84d543f66f998cd70f3d5373918
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-11 08:53:45 -0500

    finish viewProducts function and set up rest of functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -21,31 +21,24 @@ function displayOptions() {
         {
             type: "list",
             message: "What option would you like?",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"],
             name: "options"
 
         }
     ]).then(function(answer){
       
         if("View Products for Sale"){
-            viewProducts()
+           viewProducts()
         }
 
-        if("View Low Inventory"){
-            lowInv()
-        }
+       
 
-        if("Add to Inventory"){
-            addInv()
-        }
+       
 
-        if("Add New Product"){
-            addProduct()
-        }
-    })
+        
+    });
 
 
-    connection.end()
 }
 
 function viewProducts(){
@@ -53,6 +46,7 @@ function viewProducts(){
         connection.query(
             query, 
             function(err, res){
+                if(err) throw err;
                 for(var i = 0; i < res.length; i++){
                     console.log(`
                         Product ID: ${res[i].item_id} Product Name: ${res[i].product_name} Product Price: ${res[i].price}
@@ -60,6 +54,7 @@ function viewProducts(){
                 }
             }
         )
+        connection.end()
 }
 
 function lowInv(){
@@ -67,8 +62,59 @@ function lowInv(){
         connection.query(
             query,
             function(err, res){
-                for(var i = 0; i < res.length)
+                for(var i = 0; i < res.length; i++){
+                    if(stock_quantity < 5){
+                        console.log(`Item ID: ${res[i].item_id} Quantity: ${res[i].stock_quantity}`)
+                    }
+                }
             }
 
         )
 }
+
+function addInv(){
+    inquirer
+    .prompt
+    ([
+        {
+            type: "input",
+            message: "What is the item id that you want to update?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "How many items are you adding?",
+            name: "inv"
+        }
+    ]).then(function(answer){
+        var query = "UPDATE products SET ? WHERE ?";
+        var newInv = answer.inv += stock_quantity;
+        var id = answer.id
+        connection.query(
+            query, 
+            function(err, res){
+                if(err) throw err;
+              [ 
+                {
+                    stock_quantity: newInv
+                },
+                {
+                    item_id: id
+                }
+             ]
+            }
+        )
+        console.log("Your item has been updated!")
+    })
+
+   
+}
+
+function addProduct(){
+
+}
+function exit(){
+    connection.end()
+}
+
+
